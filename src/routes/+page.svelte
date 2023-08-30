316d32e4e9894ec4758f618a5e5c071ee7dfa6f5
Chandranshu <chandranshu040@gmail.com>
2023-08-30 17:36:33 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { GrowthBook } from '@growthbook/growthbook';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
